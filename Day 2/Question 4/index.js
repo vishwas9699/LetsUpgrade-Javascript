702015010bd8cc815156f29a9303ec6a643bc4c1
vishwas9699 <vvishwas179@gmail.com>
2020-08-31 20:06:49 +0530

    ic

--- Day 2/Question 4/index.js
+++ Day 2/Question 4/index.js
@@ -1,2 +1,6 @@
-A=["dog","cat","cow","goat"]
-if()
+let Animal=["dog","cat","cow","goat"];
+for(i=0;i<Animal.length;i++){
+    if(Animal[i].includes("a")){
+        console.log(Animal[i]);
+    }
+}
