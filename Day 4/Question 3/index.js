314839e883e3c5f92939979da764fbd130d5156f
vishwas9699 <vvishwas179@gmail.com>
2020-09-10 22:22:55 +0530

    change

---
+++ Day 4/Question 3/index.js
@@ -0,0 +1,30 @@
+let details =[
+    {
+        name: "Harry",
+        age: "20",
+        country: "India",
+        hobbies: ['dance','design','play']
+    },
+    {
+        name: "Sam",
+        age: "22",
+        country: "US",
+        hobbies: ['dance','watch','sing']
+    },
+    {
+        name: "Jay",
+        age: "26",
+        country: "Australia",
+        hobbies: ['eating','write','read']
+    },
+    {
+        name: "Alex",
+        age: "28",
+        country: "Russia",
+        hobbies: ['sleep','Lazy','Eating']
+    },
+];
+function display(details){
+    console.log(details);
+}
+display(details);
