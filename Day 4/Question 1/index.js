b3c2a58b8c3dd17af80f6507fa387ac05f61352d
vishwas9699 <vvishwas179@gmail.com>
2020-09-10 22:16:44 +0530

    change

---
+++ Day 4/Question 1/index.js
@@ -0,0 +1,18 @@
+  
+function first()
+{
+    const element=document.getElementsByClassName("image");
+    element[0].src='image1.jpg';
+}
+
+function second()
+{
+    const element=document.getElementsByClassName("image");
+    element[0].src='image2.jpg';
+}
+
+function third()
+{
+    const element=document.getElementsByClassName("image");
+    element[0].src='image3.jpg';
+}
---
+++ Day 4/Question 2/index.js
@@ -0,0 +1,4 @@
+function change(){
+    const element=document.getElementsByClassName("input");
+    element[1].value=element[0].value;
+}
